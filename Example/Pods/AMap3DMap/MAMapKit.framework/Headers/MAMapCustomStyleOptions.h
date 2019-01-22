7d3c7ab3e6a25dea0e4110df2d8be24f6f96b52d
xiaheng666 <key@xiaheng.net>
2019-01-22 17:21:10 +0800

    upload framework

---
+++ Example/Pods/AMap3DMap/MAMapKit.framework/Headers/MAMapCustomStyleOptions.h
@@ -0,0 +1,23 @@
+//
+//  MAMapCustomStyleOptions.h
+//  MAMapKit
+//
+//  Created by ldj on 2018/11/27.
+//  Copyright © 2018 Amap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MAMapCustomStyleOptions : NSObject
+
+///自定义样式二进制
+@property (nonatomic, strong) NSData *styleData;
+
+///设置地图自定义样式对应的styleID，从官网获取
+@property (nonatomic, strong) NSString *styleId;
+
+///设置自定义纹理文件二进制
+@property (nonatomic, strong) NSData *styleTextureData;
+
+@end
+
--- Example/Pods/Target Support Files/Pods-XVIEWAMapManager_Example/Pods-XVIEWAMapManager_Example-resources.sh
+++ Example/Pods/Target Support Files/Pods-XVIEWAMapManager_Example/Pods-XVIEWAMapManager_Example-resources.sh
@@ -91,6 +91,14 @@ EOM
       ;;
   esac
 }
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "${PODS_ROOT}/AMap3DMap/MAMapKit.framework/AMap.bundle"
+  install_resource "${PODS_ROOT}/AMapNavi/AMapNaviKit.framework/AMapNavi.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "${PODS_ROOT}/AMap3DMap/MAMapKit.framework/AMap.bundle"
+  install_resource "${PODS_ROOT}/AMapNavi/AMapNaviKit.framework/AMapNavi.bundle"
+fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
